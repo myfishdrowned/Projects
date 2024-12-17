6fea8f54d4aee946042d42d56095fd83eacad09a
myfishdrowned <saif.rahman.va@gmail.com>
2024-12-17 15:05:12 -0500

    created computer and human functions for rock paper scissors

--- projects/rock-paper-scissors/script.js
+++ projects/rock-paper-scissors/script.js
@@ -1,1 +1,32 @@
-console.log('let\'s begin with the rock paper scissors project!')
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+
+    if (randomNum < 1) {
+        return 'rock';
+    }
+    else if (randomNum < 2) {
+        return 'paper';
+    }
+    else {
+        return 'scissors';
+    }
+}
+console.log(getComputerChoice())
+
+function getHumanChoice(str) {
+    if (str === 'rock') {
+        return 'rock'
+    }
+    else if (str === 'paper') {
+        return 'paper'
+    }
+    else if (str === 'scissors') {
+        return 'scissors'
+    }
+}
+let str = prompt('rock, paper, or scissors? ')
+console.log(getHumanChoice(str))
+
+let humanScore = 0
+let computerScore = 0
+
