01207b2fafeb3c369872c36668382dc2b526465e
myfishdrowned <saif.rahman.va@gmail.com>
2024-12-18 08:38:56 -0500

    Created playRound function which plays game of rock paper scissors between user and comptuer

--- projects/rock-paper-scissors/script.js
+++ projects/rock-paper-scissors/script.js
@@ -30,3 +30,48 @@ console.log(getHumanChoice(str))
 let humanScore = 0
 let computerScore = 0
 
+function playRound(humanSelection, computerSelection) {
+    if (computerSelection === 'rock' && humanSelection === 'paper') {
+        humanScore += 1;
+        console.log('You\'ve won this round!')
+    }
+    if (computerSelection === 'rock' && humanSelection === 'rock') {
+        console.log('You\'ve tied this round.')
+    }
+    if (computerSelection === 'rock' && humanSelection === 'scissors') {
+        computerScore += 1;
+        console.log('The computer has won this round!')
+    }
+    // -----------------------------------------------------------------
+    if (computerSelection === 'paper' && humanSelection === 'paper') {
+        console.log('You\'ve tied this round.')
+    }
+    if (computerSelection === 'paper' && humanSelection === 'rock') {
+        computerScore += 1;
+        console.log('The computer has won this round!')
+    }
+    if (computerSelection === 'paper' && humanSelection === 'scissors') {
+        humanScore += 1;
+        console.log('You\'ve won this round!')
+    }
+    // -----------------------------------------------------------------
+    if (computerSelection === 'scissors' && humanSelection === 'rock') {
+        humanScore += 1;
+        console.log('You\'ve won this round!')
+    }
+    if (computerSelection === 'scissors' && humanSelection === 'paper') {
+        computerScore += 1;
+        console.log('The computer has won this round!')
+    }
+    if (computerSelection === 'scissors' && humanSelection === 'scissors') {
+        console.log('You\'ve tied this round.')
+    }
+}
+
+const humanSelection = getHumanChoice(str);
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log(`Computer's score: ${computerScore}`)
+console.log(`Your score: ${humanScore}`)
